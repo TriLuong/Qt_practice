6331089688cd9583b98aeb0aefd06635761141fa
Tri Luong <lxt0094@gmail.com>
2019-01-06 00:21:58 +0700

    Example for Message Box
    Signed-off-by: Tri Luong <lxt0094@gmail.com>

---
+++ MessageBox/mainwindow.cpp
@@ -0,0 +1,43 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pbInfor_clicked()
+{
+    QMessageBox::information(this,"Infor","Xin chao cac ban!!");
+}
+
+void MainWindow::on_pbWarning_clicked()
+{
+    QMessageBox::warning(this,"Warning","Canh bao!!");
+}
+
+
+
+void MainWindow::on_pbCritical_clicked()
+{
+    QMessageBox::critical(this,"Critical","Critical!!",QMessageBox::Ok | QMessageBox::Cancel);
+}
+
+
+void MainWindow::on_pbQuestrion_clicked()
+{
+    if(QMessageBox::question(this,"Question","Hoi cai gi?", QMessageBox::Ok | QMessageBox::Cancel)==QMessageBox::Ok){
+        this->setWindowTitle("Yes");
+    }
+    else{
+        this->setWindowTitle("No");
+    }
+}
